6b97ed263a8a6821363acd142eb4ba5342aae6bd
Mr. E-Cyber Team <111108508+teamcyber-glitch@users.noreply.github.com>
2023-08-06 00:07:07 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+pkg update && pkg upgrade && apt-get update && apt-get upgrade && pkg install cowsay && pkg install php && pkg install nmap && pkg install traceroute && pkg install whois && pkg install git && pkg install bash && pkg install python && pkg install curl && pkg install openssh && pkg install toilet && pkg install wget && pkg install figlet && pkg install ruby && pkg install nodejs && pkg install ffmpeg && pkg install neofetch && pkg install mc && pkg install nano && pkg install python2 && pkg install python3 && pkg install jq 
+pip install colorama mechanize gem lolcat clone
+gem install lolcat
+pip install -r requirements.txt 
